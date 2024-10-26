7594a1e67bb3ee427579962f21980b8b8fa042a2
Chris Johnson <6319532+Johnsoct@users.noreply.github.com>
2024-10-26 01:41:01 -0400

    Update insert_data.sh
    Stripped FCC

--- insert_data.sh
+++ insert_data.sh
@@ -2,9 +2,9 @@
 
 if [[ $1 == "test" ]]
 then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+  PSQL="psql --username=<testuser> --dbname=<testdb> -t --no-align -c"
 else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+  PSQL="psql --username=<user> --dbname=<db> -t --no-align -c"
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
